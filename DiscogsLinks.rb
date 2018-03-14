a32deb7069bbee42691d621a536ba4fb5899022b
Eugene <s9th.main@gmail.com>
2018-03-14 15:53:32 +0200

    Pass class as a parameter

--- DiscogsLinks.rb
+++ DiscogsLinks.rb
@@ -9,22 +9,16 @@ require_relative 'providers/BandcampRelease.rb'
 LinksNumber = 5
 
 def getmusiclinks(provider)
-    case provider
-    when "discogs"
-        DiscogsRelease.random_release(LinksNumber).map{|release| release.to_s}.join("\n")
-    when "bandcamp"
-        BandcampRelease.random_release(LinksNumber).map{|release| release.to_s}.join("\n")
-    end
+    provider.random_release(LinksNumber).map{|release| release.to_s}.join("\n")
 end
-
 
 Telegram::Bot::Client.run(ENV["TelegramToken"]) do |bot|
     bot.listen do |message|
         case message.text
         when '/music'
-            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks("discogs"), parse_mode: 'HTML', disable_web_page_preview: true)
+            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(DiscogsRelease), parse_mode: 'HTML', disable_web_page_preview: true)
         when '/music bc'
-            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks("bandcamp"), parse_mode: 'HTML', disable_web_page_preview: true)
+            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(BandcampRelease), parse_mode: 'HTML', disable_web_page_preview: true)
         end
     end
 end
