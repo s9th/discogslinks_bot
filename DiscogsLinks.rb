8ecda1b482392c1e29bc601774f3a581d709c2ee
Eugene <s9th.main@gmail.com>
2018-03-14 17:30:34 +0200

    Changed response to provider relations

--- DiscogsLinks.rb
+++ DiscogsLinks.rb
@@ -7,22 +7,17 @@ require_relative 'providers/DiscogsRelease.rb'
 require_relative 'providers/BandcampRelease.rb'
 
 LinksNumber = 5
+commandMapper = { '/music' => DiscogsRelease,'/music bc' => BandcampRelease }
 
 def getmusiclinks(provider)
     provider.random_release(LinksNumber).map{|release| release.to_s}.join("\n")
 end
 
-def sendmsg(bot, message, provider)
-    bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(provider), parse_mode: 'HTML', disable_web_page_preview: true)
-end
-
 Telegram::Bot::Client.run(ENV["TelegramToken"]) do |bot|
     bot.listen do |message|
-        case message.text
-        when '/music'
-            sendmsg(bot, message, DiscogsRelease)
-        when '/music bc'
-            sendmsg(bot, message, BandcampRelease)
+        provider = commandMapper[message.text]
+        if (provider)
+            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(provider), parse_mode: 'HTML', disable_web_page_preview: true)
         end
     end
 end
