4f0fde632d81f48c3dccfc8ba1993c9d1495af7c
Eugene <s9th.main@gmail.com>
2018-03-14 16:36:40 +0200

    fixed a typo

--- DiscogsLinks.rb
+++ DiscogsLinks.rb
@@ -12,7 +12,7 @@ def getmusiclinks(provider)
     provider.random_release(LinksNumber).map{|release| release.to_s}.join("\n")
 end
 
-def sendmsg (bot, provider)
+def sendmsg(bot, provider)
     bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(provider), parse_mode: 'HTML', disable_web_page_preview: true)
 end
 
