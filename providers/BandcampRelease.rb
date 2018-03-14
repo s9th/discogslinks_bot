9877cd6289a65e72ec4b2d46fd6ebbc20cc467f2
Eugene <s9th.main@gmail.com>
2018-03-14 18:27:53 +0200

    Added bandcamp validator

--- providers/BandcampRelease.rb
+++ providers/BandcampRelease.rb
@@ -7,22 +7,36 @@ class BandcampRelease
     random_releases = []
     while i < n
       release = self.new
-      random_releases << release
-      i+=1
+      if release.valid?
+        random_releases << release
+        i+=1
+      end
     end
     random_releases
   end
 
+  # release is valid if it does not redirect to another page
+  def valid?
+    page = HTTParty.get(url)
+    url == page.request.last_uri.to_s
+  end
+
+  # we get a random url here from the randomizer and change it to https
   def random_url
     page = HTTParty.get(HostURL)
     doc = Nokogiri.parse(page.body)
     links = doc.css('a')
     links = links.map { |link| link.attribute('href').to_s }.uniq.sort.delete_if { |h| !h.include?(".bandcamp") }
-    links.first.strip
+    links.first.strip.sub("http","https")
   end
 
+  # do it once because otherwise it will repeat itself
+  def url
+    @url ||= random_url
+  end
+  
   def to_s
-    random_url
+    url
   end
 
 end
