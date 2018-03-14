51a4f980040498a2b9c1dcd24bce54c015f73ede
Eugene <s9th.main@gmail.com>
2018-03-14 16:30:39 +0200

    Third refactoring attempt

--- DiscogsLinks.rb
+++ DiscogsLinks.rb
@@ -16,9 +16,14 @@ Telegram::Bot::Client.run(ENV["TelegramToken"]) do |bot|
     bot.listen do |message|
         case message.text
         when '/music'
-            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(DiscogsRelease), parse_mode: 'HTML', disable_web_page_preview: true)
+            sendmsg(DiscogsRelease)
         when '/music bc'
-            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(BandcampRelease), parse_mode: 'HTML', disable_web_page_preview: true)
+            sendmsg(BandcampRelease)
         end
     end
+
+    def sendmsg(provider)
+        bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(provider), parse_mode: 'HTML', disable_web_page_preview: true)
+    end
+
 end
