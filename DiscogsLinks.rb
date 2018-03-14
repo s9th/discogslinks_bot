ed989530187b754318cfe869e8d6005431327504
Eugene <s9th.main@gmail.com>
2018-03-14 16:24:17 +0200

    Refactored again, now properly

--- DiscogsLinks.rb
+++ DiscogsLinks.rb
@@ -13,12 +13,14 @@ def getmusiclinks(provider)
 end
 
 Telegram::Bot::Client.run(ENV["TelegramToken"]) do |bot|
+    provider = nil
     bot.listen do |message|
         case message.text
         when '/music'
-            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(DiscogsRelease), parse_mode: 'HTML', disable_web_page_preview: true)
+            provider = DiscogsRelease
         when '/music bc'
-            bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(BandcampRelease), parse_mode: 'HTML', disable_web_page_preview: true)
+            provider = BandcampRelease
         end
     end
+    bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(provider), parse_mode: 'HTML', disable_web_page_preview: true) unless provider == nil
 end
