436dcc912f48bd1959e7019751df8c6c72afbde8
Eugene <s9th.main@gmail.com>
2018-03-14 16:41:49 +0200

    pass message to the sendmessage

--- DiscogsLinks.rb
+++ DiscogsLinks.rb
@@ -12,7 +12,7 @@ def getmusiclinks(provider)
     provider.random_release(LinksNumber).map{|release| release.to_s}.join("\n")
 end
 
-def sendmsg(bot, provider)
+def sendmsg(bot, message, provider)
     bot.api.send_message(chat_id: message.chat.id, text: getmusiclinks(provider), parse_mode: 'HTML', disable_web_page_preview: true)
 end
 
@@ -20,9 +20,9 @@ Telegram::Bot::Client.run(ENV["TelegramToken"]) do |bot|
     bot.listen do |message|
         case message.text
         when '/music'
-            sendmsg(bot, DiscogsRelease)
+            sendmsg(bot, message, DiscogsRelease)
         when '/music bc'
-            sendmsg(bot, BandcampRelease)
+            sendmsg(bot, message, BandcampRelease)
         end
     end
 end
